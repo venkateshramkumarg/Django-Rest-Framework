b7468bd35f681f4df19457c4f8bede3fe5d92c1c
venkateshramkumarg <venkateshramkumar.g2021csec@sece.ac.in>
2025-01-23 14:26:46 +0530

    Update .gitignore to exclude all SQLite database files and add commented-out model definitions in blog/models.py

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -13,3 +13,23 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# class User(models.Model):
+#     id = models.CharField(max_length=25, primary_key=True, default=generate_cuid)
+#     role = models.JSONField(default=default_role)
+
+# class Post(models.Model):
+#     id = models.CharField(max_length=25, primary_key=True, default=generate_cuid)
+#     title = models.CharField(max_length=100)
+#     content = models.CharField(max_length=5000)
+#     likes = models.IntegerField(default=0)
+#     dislikes = models.IntegerField(default=0)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+#     liked_by = models.ManyToManyField(User, related_name='liked_posts')
+#     disliked_by = models.ManyToManyField(User, related_name='disliked_posts')
+
+# class Comment(models.Model):
+#     id = models.CharField(max_length=25, primary_key=True, default=generate_cuid)
+#     content = models.CharField(max_length=1000)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
